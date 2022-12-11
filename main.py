a8a79b32a69b548bd5687c377244c38d9b34b652
Rudy Casilla <94497674+rpccode@users.noreply.github.com>
2022-12-11 18:22:10 +0100

    s

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("5926816130:AAF0YddnlJ64rj4OxpX3VtT-0BNIA_TtN5A")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
