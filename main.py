a6c708b3ca3a3adbe2dc5ae64feec7a814f7427f
Rudy Casilla <94497674+rpccode@users.noreply.github.com>
2022-12-12 16:50:17 +0000

    sql

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import telebot
 
 bot = telebot.TeleBot("5926816130:AAF0YddnlJ64rj4OxpX3VtT-0BNIA_TtN5A")
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing?")
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
+
 
 bot.infinity_polling()
